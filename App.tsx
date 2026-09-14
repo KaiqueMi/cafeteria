9c001255dd63fa1766784ecb77e55d173e212bdb
Kaique <mirandakaique740@gmail.com>
2026-09-14 17:07:15 -0300

    feat: adicionando interação

--- App.tsx
+++ App.tsx
@@ -1,8 +1,12 @@
 import { Ionicons } from '@expo/vector-icons';
-import { StyleSheet, Text, View, ScrollView, Image, } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInputComponent, TouchableOpacity, TextInputBase, TextInput, KeyboardAvoidingView, } from 'react-native';
+
 
 export default function App() {
   return (
+    <KeyboardAvoidingView style={styles.container}
+    behavior="padding"
+    keyboardVerticalOffset={30}>
     <ScrollView style={styles.container}>
       {/* Header */}
       <View style={styles.header}>
@@ -24,17 +28,60 @@ export default function App() {
         <Text style={styles.grettingSubtitle}>Que tal um café hoje?</Text>
       </View>
 
-      <View>
-        <Image source={require('./assets/coffee.jpg')}></Image>
-        <Text>Cappucino Especial</Text>
-        <Text>Cremoso e delicioso</Text>
-        <Text>R$ 12,90</Text>      
+      <View style={styles.featured}>
+        <Image style ={styles.image} source={require('./assets/coffee.jpg')}></Image>
+        <Text style={styles.featuredTitle}>Cappucino Especial</Text>
+        <Text style={styles.featuredDescription}>Cremoso e delicioso</Text>
+        <Text style={styles.featuredPrice}>R$ 12,90</Text>      
       </View>
+
+
+      <Text style={styles.sectionTitle}>Nosso Cardápio</Text>
+    
+
+      <View style={styles.menu}>
+        <View style={styles.miniFeatured}>
+          <Text style={styles.produtoTitle}>Espresso</Text>
+          <Text style={styles.produtoDescription}>Puro e forte</Text>
+          <Text style={styles.produtoPrice}>R$ 7,00</Text>
+        </View>
+
+        <View style={styles.miniFeatured}>
+          <Text style={styles.produtoTitle}>Cappuccino</Text>
+          <Text style={styles.produtoDescription}>Clássico com espuma</Text>
+          <Text style={styles.produtoPrice}>R$ 12,90</Text>
+        </View>
+
+        <View style={styles.miniFeatured}>
+          <Text style={styles.produtoTitle}>Latte</Text>
+          <Text style={styles.produtoDescription}>Leite cremoso</Text>
+          <Text style={styles.produtoPrice}>R$ 11,50</Text>
+        </View>
+
+        <View style={styles.miniFeatured}>
+          <Text style={styles.produtoTitle}>Mocha</Text>
+          <Text style={styles.produtoDescription}>Toque de chocolate</Text>
+          <Text style={styles.produtoPrice}>R$ 13,50</Text>
+        </View>        
+      </View>
+
+
+      <View style={styles.orderSection}>
+        <Text style={styles.question}>Qual é o seu nome?</Text>
+        
+        <TextInput style={styles.input} placeholder="Digite seu nome"></TextInput>
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Fazer meu pedido</Text>
+        </TouchableOpacity>
+      </View>
+
 
     </View>
     
 
     </ScrollView>
+   </KeyboardAvoidingView>
   );
 }
 
@@ -96,5 +143,132 @@ const styles = StyleSheet.create({
     marginTop: 8
   },
 
+   featured: {
+    backgroundColor: "#ffffff",
+    padding: 16,
+    borderRadius: 24,
+    shadowColor: "#000000",
+    shadowOffset: {width:0, height: 8},
+    shadowOpacity: 0.05,
+    elevation: 4,
+    marginBottom: 32
+  },
 
-})
+  image: {
+    width: "100%",
+    height: 180,
+    marginBottom: 16,
+    borderRadius: 16
+  },
+
+  featuredTitle: {
+    fontSize: 20,
+    fontWeight: "800",
+    color: "#2f2d2c",
+  },
+
+  featuredDescription: {
+    fontSize: 14,
+    color: "#9b9b9b",
+    marginTop: 4
+  },
+
+  featuredPrice: {
+    fontSize: 20,
+    fontWeight: "800",
+    color: "#c67c4e",
+    marginTop: 12
+  },
+
+  sectionTitle: {
+    fontSize: 22,
+    fontWeight: "800",
+    color: "#2f2d2c",
+    marginBottom: 16
+  },
+
+  miniFeatured: {
+    backgroundColor: "#ffffff",
+    padding: 16,
+    borderRadius: 16,
+    shadowColor: "#000000",
+    shadowOffset: {width:0, height: 4},
+    shadowOpacity: 0.05,
+    elevation: 3,
+    width: "48%",
+    marginBottom: 16,
+  },
+
+  produtoTitle: {
+    fontSize: 16,
+    fontWeight: "700",
+  },
+
+  produtoDescription: {
+    fontSize: 12,
+    marginTop: 4
+  },
+
+  produtoPrice: {
+    fontSize: 16,
+    fontWeight: "800",
+    marginTop: 12,
+    color: "#c67c4e"
+  },
+
+  menu: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginBottom: 32
+  },
+
+  orderSection: {
+    backgroundColor: "#ffffff",
+    padding: 24,
+    borderRadius: 24,
+    shadowColor: "#000000",
+    shadowOffset: {width:0, height:8},
+    shadowOpacity: 0.05,
+    elevation: 4,
+    marginTop: 10
+  },
+
+  question: {
+    fontSize: 18,
+    fontWeight: "800",
+    color: "#2f2d2c",
+    marginBottom: 16
+  },
+
+  input: {
+    width: "100%",
+    height: 56,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 16,
+    paddingHorizontal: 20,
+    fontSize: 16,
+  },
+
+  button: {
+    width: "100%",
+    backgroundColor: "#c67c4e",
+    borderRadius: 30,
+    paddingVertical: 16,
+    paddingHorizontal: 30,
+    alignItems: "center",
+    marginTop: 20,
+    shadowColor: "#c67c4e",
+    shadowOffset: {width: 0, height: 4},
+    shadowOpacity: 0.05,
+    shadowRadius: 10,
+    elevation: 4
+  },
+
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "700",
+    color: "#ffffff",
+  },
+
+});
